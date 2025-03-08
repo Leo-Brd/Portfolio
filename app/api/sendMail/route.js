c8042588ea2264fb023cf7cdaf17d09f1cbc8756
Leo-Brd <leobordet.pro@gmail.com>
2025-03-09 00:47:30 +0100

    Fix email spamming using rate limiter flexible

--- app/api/sendMail/route.js
+++ app/api/sendMail/route.js
@@ -1,10 +1,21 @@
 import { NextResponse } from 'next/server';
 import sgMail from '@sendgrid/mail';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+const rateLimiter = new RateLimiterMemory({
+  points: 3,
+  duration: 60,
+});
+
 export async function POST(request) {
+  const ip = request.headers.get('x-forwarded-for') || request.ip;
+
   try {
+
+    await rateLimiter.consume(ip);
+
     const { name, email, message } = await request.json();
 
     const msg = {
@@ -23,6 +34,13 @@ export async function POST(request) {
     );
   } catch (error) {
     console.error(error);
+
+    if (error instanceof Error && error.message === 'Rate limiter exceeded') {
+      return NextResponse.json(
+        { error: 'Trop de requêtes. Veuillez réessayer plus tard.' },
+        { status: 429 }
+      );
+    }
 
     return NextResponse.json(
       { error: 'Erreur lors de l\'envoi du mail' },
