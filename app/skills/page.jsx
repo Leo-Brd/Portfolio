60b97afe82a32990ab6a8154f8e323166c515839
Leo-Brd <leobordet.pro@gmail.com>
2025-03-10 01:47:38 +0100

    Enhance skills page responsive

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useTranslation } from 'next-i18next';
-import { ToolIcon } from "@/components/ui/toolIcon"
+import { ToolIcon } from "@/components/ui/toolIcon";
 import {
   SiHtml5,
   SiCss3,
@@ -31,29 +31,29 @@ import { CiMobile1 } from "react-icons/ci";
 import { RiNotionFill } from "react-icons/ri";
 
 const tools = [
-  { icon: <SiHtml5 className="w-[40px] h-[40px] text-[#E34F26]" />, name: "HTML5", category: "Langages" },
-  { icon: <SiCss3 className="w-[40px] h-[40px] text-[#1572B6]" />, name: "CSS3", category: "Langages" },
-  { icon: <SiJavascript className="w-[40px] h-[40px] text-[#F7DF1E]" />, name: "JavaScript", category: "Langages" },
-  { icon: <SiPython className="w-[40px] h-[40px] text-[#3776AB]" />, name: "Python", category: "Langages" },
-  { icon: <SiC className="w-[40px] h-[40px] text-[#226db3]" />, name: "C", category: "Langages" },
-  { icon: <SiSass className="w-[40px] h-[40px] text-[#CC6699]" />, name: "Sass", category: "Langages" },
-  { icon: <TbBrandVscode className="w-[40px] h-[40px] text-[#007ACC]" />, name: "VS Code", category: "Outils de développement" },
-  { icon: <SiGithub className="w-[40px] h-[40px] text-black dark:text-white" />, name: "GitHub", category: "Outils de développement" },
-  { icon: <SiGit className="w-[40px] h-[40px] text-[#F05032]" />, name: "Git", category: "Outils de développement" },
-  { icon: <IoTerminal className="w-[40px] h-[40px] text-black dark:text-[#ffffff]" />, name: "Terminal", category: "Outils de développement" },
-  { icon: <img src={ChatGPTLogo.src} alt="Logo de ChatGPT" className="w-[40px] h-[40px] text-[#3bc5ba]" />, name: "IA", category: "Outils de développement" },
-  { icon: <FaChrome className="w-[40px] h-[40px] text-[#237aff]" />, name: "Chrome Devtools", category: "Outils de développement" },
-  { icon: <SiFigma className="w-[40px] h-[40px] text-[#F24E1E]" />, name: "Figma", category: "Design" },
-  { icon: <SiCanva className="w-[40px] h-[40px] text-[#3bc5ba]" />, name: "Canva", category: "Design" },
-  { icon: <CiMobile1 className="w-[40px] h-[40px] text-black dark:text-white" />, name: "Responsive Design", category: "Design" },
-  { icon: <SiReact className="w-[40px] h-[40px] text-[#61DAFB]" />, name: "React", category: "Frameworks" },
-  { icon: <SiNodedotjs className="w-[40px] h-[40px] text-[#339933]" />, name: "Node.js Express", category: "Frameworks" },
-  { icon: <SiMongodb className="w-[40px] h-[40px] text-[#47A248]" />, name: "MongoDB", category: "Bases de données" },
-  { icon: <TbSeo className="w-[40px] h-[40px] text-[#0D6EFD]" />, name: "Référencement", category: "Autres" },
-  { icon: <BiHandicap className="w-[40px] h-[40px] text-black dark:text-white" />, name: "Accessibilité", category: "Autres" },
-  { icon: <VscDebugAll className="w-[40px] h-[40px] text-[#DC3545]" />, name: "Debug", category: "Autres" },
-  { icon: <SiFeedly className="w-[40px] h-[40px] text-[#3ba83b]" />, name: "Feedly (veille technologique)", category: "Autres" },
-  { icon: <RiNotionFill className="w-[40px] h-[40px] text-black dark:text-white" />, name: "Notion", category: "Autres" },
+  { icon: <SiHtml5 className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#E34F26]" />, name: "HTML5", category: "Langages" },
+  { icon: <SiCss3 className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#1572B6]" />, name: "CSS3", category: "Langages" },
+  { icon: <SiJavascript className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#F7DF1E]" />, name: "JavaScript", category: "Langages" },
+  { icon: <SiPython className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#3776AB]" />, name: "Python", category: "Langages" },
+  { icon: <SiC className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#226db3]" />, name: "C", category: "Langages" },
+  { icon: <SiSass className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#CC6699]" />, name: "Sass", category: "Langages" },
+  { icon: <TbBrandVscode className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#007ACC]" />, name: "VS Code", category: "Outils de développement" },
+  { icon: <SiGithub className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-black dark:text-white" />, name: "GitHub", category: "Outils de développement" },
+  { icon: <SiGit className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#F05032]" />, name: "Git", category: "Outils de développement" },
+  { icon: <IoTerminal className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-black dark:text-[#ffffff]" />, name: "Terminal", category: "Outils de développement" },
+  { icon: <img src={ChatGPTLogo.src} alt="Logo de ChatGPT" className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#3bc5ba]" />, name: "IA", category: "Outils de développement" },
+  { icon: <FaChrome className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#237aff]" />, name: "Chrome Devtools", category: "Outils de développement" },
+  { icon: <SiFigma className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#F24E1E]" />, name: "Figma", category: "Design" },
+  { icon: <SiCanva className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#3bc5ba]" />, name: "Canva", category: "Design" },
+  { icon: <CiMobile1 className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-black dark:text-white" />, name: "Responsive Design", category: "Design" },
+  { icon: <SiReact className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#61DAFB]" />, name: "React", category: "Frameworks" },
+  { icon: <SiNodedotjs className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#339933]" />, name: "Node.js Express", category: "Frameworks" },
+  { icon: <SiMongodb className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#47A248]" />, name: "MongoDB", category: "Bases de données" },
+  { icon: <TbSeo className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#0D6EFD]" />, name: "Référencement", category: "Autres" },
+  { icon: <BiHandicap className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-black dark:text-white" />, name: "Accessibilité", category: "Autres" },
+  { icon: <VscDebugAll className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#DC3545]" />, name: "Debug", category: "Autres" },
+  { icon: <SiFeedly className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-[#3ba83b]" />, name: "Feedly (veille technologique)", category: "Autres" },
+  { icon: <RiNotionFill className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] text-black dark:text-white" />, name: "Notion", category: "Autres" },
 ];
 
 const mainSkills = [
@@ -78,7 +78,7 @@ export default function Skills() {
     : tools.filter((tool) => tool.category === selectedCategory);
 
   return (
-    <section className="py-20 w-full max-w-6xl mx-auto my-8">
+    <section className="py-20 w-full max-w-6xl mx-auto my-8 px-4">
       {/* Compétences principales */}
       <motion.div
         initial={{ opacity: 0, y: 50 }}
@@ -89,14 +89,16 @@ export default function Skills() {
         <h2 className="text-4xl md:text-5xl font-bold mb-12 text-center text-foreground">
           Mes Compétences Principales
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
           {mainSkills.map((skill, index) => (
             <motion.div
               key={index}
               initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.6, delay: 0.2 * index }}
-              className="p-8 bg-card border border-border rounded-2xl shadow-lg flex flex-col items-center text-center hover:shadow-xl dark:shadow-white/10 transition-shadow"
+              className={`p-8 bg-card border border-border rounded-2xl shadow-lg flex flex-col items-center text-center hover:shadow-xl dark:shadow-white/10 transition-shadow ${
+                index === 2 ? "sm:col-span-2 lg:col-span-1" : ""
+              }`}
             >
               <div className="w-20 h-20 mb-6 flex items-center justify-center text-6xl">
                 {skill.icon}
@@ -137,7 +139,7 @@ export default function Skills() {
         </div>
 
         {/* Outils filtrés */}
-        <div className="relative h-40">
+        <div className="relative h-auto">
           <div className="flex flex-wrap gap-6 justify-start items-start align-content-start">
             {filteredTools.map((tool, index) => (
               <ToolIcon key={index} tool={tool} index={index} />
@@ -156,14 +158,16 @@ export default function Skills() {
         <h2 className="text-4xl md:text-5xl font-bold mb-12 text-center text-foreground">
           En Cours d'Apprentissage...
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 justify-items-center">
           {learningSkills.map((skill, index) => (
             <motion.div
               key={index}
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.2 * index }}
-              className="p-8 bg-card border border-border rounded-2xl shadow-lg flex flex-col items-center text-center hover:shadow-xl dark:shadow-white/10 transition-shadow"
+              className={`p-8 bg-card border border-border rounded-2xl shadow-lg flex flex-col items-center text-center hover:shadow-xl dark:shadow-white/10 transition-shadow ${
+                index === 2 ? "sm:col-span-2" : ""
+              }`}
             >
               <div className="w-20 h-20 mb-6 flex items-center justify-center text-6xl">
                 {skill.icon}
