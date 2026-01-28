c3d9c0505c0d97e4b4de16500a12ab127406d7e5
l.bordet <l.bordet@seiprascore.com>
2026-01-28 10:55:52 +0100

    refacto(layout): ts errors #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 "use client"
 import "./globals.css";
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import PopUp from "@/components/info-popup"
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/i18n';
-import { FaCode } from "react-icons/fa";
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  readonly children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <I18nextProvider i18n={i18n}>
       <html lang={i18n.language}>
@@ -23,10 +26,18 @@ export default function RootLayout({ children }) {
 
           <link rel="icon" type="image/png" href="/logos/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
+          {/*
+            WARNING: Custom fonts not added in pages/_document.js will only load for a single page. This is discouraged.
+            See: https://nextjs.org/docs/messages/no-page-custom-font
+          */}
           <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
+          {/*
+            WARNING: Custom fonts not added in pages/_document.js will only load for a single page. This is discouraged.
+            See: https://nextjs.org/docs/messages/no-page-custom-font
+          */}
           <link href="https://fonts.googleapis.com/css2?family=Taviraj:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
 
           { /*OpenGraph Meta Tags*/ }
